6c3bc6e6670f4741a761d36b4f28dd2bb5b1412d
yasinhajilou <yasinhaji78@yahoo.com>
2023-12-15 05:20:17 -0500

    Added: new set oc colors and theme

--- app/src/main/java/com/example/lazylistsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/lazylistsample/ui/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.lazylistsample.theme.AppTheme
 import com.example.lazylistsample.AppNavHost
-import com.example.lazylistsample.theme.LazyListSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LazyListSampleTheme {
+            AppTheme {
                 AppNavHost()
             }
         }
