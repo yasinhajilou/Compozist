6c7072360009851e1d82af86e099cce17151af70
yasinhajilou <yasinhaji78@yahoo.com>
2023-12-16 07:38:45 -0500

    modified: nav bar padding for next btn

--- app/src/main/java/com/example/lazylistsample/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/lazylistsample/ui/welcome/WelcomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
@@ -89,8 +90,7 @@ fun BottomDetails(
     onNextButtonClicked: () -> Unit,
 ) {
     Column(
-        modifier =
-        modifier
+        modifier = modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -101,7 +101,8 @@ fun BottomDetails(
             Modifier
                 .weight(3f)
                 .fillMaxSize()
-                .background(color = MaterialTheme.colorScheme.primaryContainer),
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .navigationBarsPadding(),
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
