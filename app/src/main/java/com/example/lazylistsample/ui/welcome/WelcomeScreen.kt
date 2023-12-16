8e4a789e67298c0309d8ce50ecf0b8bc2adffc91
yasinhajilou <yasinhaji78@yahoo.com>
2023-12-16 11:31:40 -0500

    Modified: WelcomeScreen's progress indicator size

--- app/src/main/java/com/example/lazylistsample/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/lazylistsample/ui/welcome/WelcomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
@@ -148,7 +149,7 @@ fun NextButton(onButtonClicked: () -> Unit) {
         if (showLoading) {
             CircularProgressIndicator(
                 color = MaterialTheme.colorScheme.onPrimary,
-                modifier = Modifier.align(CenterVertically),
+                modifier = Modifier.size(24.dp).align(CenterVertically),
             )
         } else {
             Text(
